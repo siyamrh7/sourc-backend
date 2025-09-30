cd1d15b414499ebc4c734a867cc0979b277d182c
siyamrh7 <siyamrh7@gmail.com>
2025-09-30 16:12:53 +0600

    fix

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const authRoutes = require('./routes/authRoutes');
 const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4001;
 
 // CORS configuration
 const corsOptions = {
